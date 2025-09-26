d2687193cd89ccae7ef88e544576985c81ebb185
Edwin van Loon <emm.van.loon@belastingdienst.nl>
2025-09-26 10:52:26 +0200

    Create deltas and done streams

--- src/main/java/workshop/CustomerSupportAgentWebSocket.java
+++ src/main/java/workshop/CustomerSupportAgentWebSocket.java
@@ -4,6 +4,9 @@ import io.quarkus.websockets.next.OnOpen;
 import io.quarkus.websockets.next.OnTextMessage;
 import io.quarkus.websockets.next.WebSocket;
 import io.smallrye.mutiny.Multi;
+import jakarta.json.Json;
+
+import java.util.UUID;
 
 @WebSocket(path = "/customer-support-agent")
 public class CustomerSupportAgentWebSocket {
@@ -14,13 +17,30 @@ public class CustomerSupportAgentWebSocket {
         this.customerSupportAgent = customerSupportAgent;
     }
 
-    @OnOpen
-    public String onOpen() {
-        return "Welcome to Miles of Smiles! How can I help you today?";
-    }
-
     @OnTextMessage
     public Multi<String> onTextMessage(String message) {
-        return customerSupportAgent.chat(message);
+        // Unique id per request.
+        final var messageId = UUID.randomUUID().toString();
+
+        // Receiving tokens (over time) from LLM.
+        final var tokens = customerSupportAgent.chat(message);
+
+        // Construct delta (a stream of tokens received from the LLM asynchronously).
+        final var deltas = tokens.onItem().transform(token -> Json.createObjectBuilder()
+                .add("type", "delta")
+                .add("id", messageId)
+                .add("content", token)
+                .build()
+                .toString());
+        // Construct done (a simple mutiny object with a done flag)
+        final var done = Multi.createFrom().item(Json.createObjectBuilder()
+                .add("type", "done")
+                .add("id", messageId)
+                .build()
+                .toString());
+
+        // This is where the magic happens. Mutiny concatenates the streams in order.
+        // So it handles the deltas first until all tokens are processed, then it will send the done.
+        return Multi.createBy().concatenating().streams(deltas, done);
     }
 }
