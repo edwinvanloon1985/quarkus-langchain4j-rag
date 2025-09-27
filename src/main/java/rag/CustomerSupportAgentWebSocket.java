bbe160c0fc29a8bca41b8c2d86d1ac328d330019
Edwin van Loon <emm.van.loon@belastingdienst.nl>
2025-09-27 22:27:55 +0200

    Voeg retry mechanisme toe in geval van een status 429 van de LLM

--- src/main/java/rag/CustomerSupportAgentWebSocket.java
+++ src/main/java/rag/CustomerSupportAgentWebSocket.java
@@ -5,6 +5,7 @@ import io.quarkus.websockets.next.WebSocket;
 import io.smallrye.mutiny.Multi;
 import jakarta.json.Json;
 
+import java.time.Duration;
 import java.util.UUID;
 
 @WebSocket(path = "/customer-support-agent")
@@ -16,7 +17,11 @@ public record CustomerSupportAgentWebSocket(CustomerSupportAgent customerSupport
         final var messageId = UUID.randomUUID().toString();
 
         // Receiving tokens (over time) from LLM.
-        final var tokens = customerSupportAgent.chat(message);
+        final var tokens = customerSupportAgent.chat(message)
+                .onFailure(this::isStatus429)
+                .retry()
+                .withBackOff(Duration.ofSeconds(1), Duration.ofSeconds(5))
+                .atMost(3);
 
         // Construct delta (a stream of tokens received from the LLM asynchronously).
         final var deltas = tokens.onItem().transform(token -> Json.createObjectBuilder()
@@ -35,5 +40,10 @@ public record CustomerSupportAgentWebSocket(CustomerSupportAgent customerSupport
         // This is where the magic happens. Mutiny concatenates the streams in order.
         // So it handles the deltas first until all tokens are processed, then it will send the done.
         return Multi.createBy().concatenating().streams(deltas, done);
+    }
+
+    private boolean isStatus429(final Throwable throwable) {
+        return throwable instanceof jakarta.ws.rs.WebApplicationException
+                && ((jakarta.ws.rs.WebApplicationException) throwable).getResponse().getStatus() == 429;
     }
 }
